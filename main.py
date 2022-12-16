5f41b284480d12d47525e8f119a02b00b867d3de
Fouad Mahmoud <100407343+fouadmahmoud281@users.noreply.github.com>
2022-12-16 17:34:04 +0000

    make it more usable

--- main.py
+++ main.py
@@ -1,16 +1,30 @@
 print("welcome to the adventure park")
 agree =str(input("would you like to try rollercoaster (y/n) "))
+photo =str(input("do you want a photo? (y/n) "))
+bill = 0
 if agree == "y":
   height= int(input("enter your height please in cm "))
   age = int(input("enter your age please : "))
-  if age >= 18 :
-      print("your tickets will cost 20$")
-  else:
-      print("your ticket will cost 10$")
-  if height >= 120 :
+  if age >= 18 and age < 60:
+      
+      bill = 20
+  elif age < 18 and age > 10:
+    
+    bill =10
+  elif age < 10:
+    print("you are under age")
+  elif age >= 60:
+      print("you must go to hospital grandpa")
+  if photo == "y":
+    bill +=3 
+    print(f"your final bill {bill} $ , included a photo")
+  else :
+    print(f"your final bill {bill} , not icluded a photo")
+  if height >= 120 and age < 60 and age > 10 :
     print("here are your tickets")
   else:
     print("sorry you can try other game")
+  
   
 else:
   other_game =str(input("what you would like to play  "))
